3a80fcaa55f1fc0da5b6d396d7939ffd669535dd
Vender <rodionygrinovich@gmail.com>
2018-04-16 20:32:28 +0300

    fixed

--- Array/Array/ViewController.m
+++ Array/Array/ViewController.m
@@ -22,11 +22,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     //Create NSArray, containing several strings, using literal declaration.
     NSArray *strings = @[@"1", @"2", @"3", @"4", @"$"];
     
     //Create mutable array from piviously created NSArray.
-    NSMutableArray *mutStrings = [NSMutableArray arrayWithArray:strings];//1
+    NSMutableArray *mutStrings = [NSMutableArray arrayWithArray:strings];
     
     //Create an empty array and obtain its first and last element in a safe way.
     NSArray *empty = [NSArray array];
@@ -47,15 +48,17 @@
     NSArray *trueDeepCopyForStrings = [NSKeyedUnarchiver unarchiveObjectWithData:[NSKeyedArchiver archivedDataWithRootObject:stringsFrom1To20]];
     
     //Got the element at index 13.
-    for(int i = 0; i < [trueDeepCopyForStrings count]; i++) {
-        if(i == 13) {
-            NSLog(@"%@", trueDeepCopyForStrings[i]);
+    [trueDeepCopyForStrings enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        if(idx == 13) {
+            NSLog(@"%@", obj);
+            *stop = YES;
         }
-    }
+    }];
+    
     
     //Make stringsFrom1To20 mutable.
     NSMutableArray *mutStringsFrom1To20 = [stringsFrom1To20 mutableCopy];
-    if([mutStringsFrom1To20 isKindOfClass: [NSArray class]]) {
+    if([mutStringsFrom1To20 isKindOfClass:[NSMutableArray class]]) {
         NSLog(@"YES");
     } else {
         NSLog(@"NO");
@@ -65,18 +68,24 @@
     [mutStringsFrom1To20 addObject:@"I ADDED YOU"];
     [mutStringsFrom1To20 insertObject:@"I Added you" atIndex:0];
     
-    //Iterated over an array and removed element at index 5
-    for(int i = 0; i < [mutStringsFrom1To20 count]; i++) {
-        if(i == 5) {
-            [mutStringsFrom1To20 removeObjectAtIndex:i];
+   
+     //Iterated over an array and removed element at index 5
+    [mutStringsFrom1To20 enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        if(idx == 5) {
+            [mutStringsFrom1To20 removeObjectAtIndex: idx];
+            *stop = YES;
         }
-    }
+    }];
+    NSLog(@"%@", mutStringsFrom1To20);
+    
+    
     //Array with mixed numbers
     NSMutableArray *numbers = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInteger:4],[NSNumber numberWithInteger:2],[NSNumber numberWithInteger:9],[NSNumber numberWithInteger:3], nil];
+    NSLog(@"%@", numbers);
     
     //Sorted array with sortUsingComparator method:
-    [numbers sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-        if(obj1 < obj2) {
+    [numbers sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        if(obj1 > obj2) {
             return (NSComparisonResult)NSOrderedDescending;
         } else {
             return 0;
@@ -85,7 +94,6 @@
     NSLog(@"%@", numbers);
     
     
-    NSLog(@"\nstrings = %lu\nmutStrings = %d\nempty = %d\nstringsFrom1To20 = %d\nshallowCopyForStrings = %d\noneLevelDeepCopy = %d\ntrueDeepCopyForStrings = %d\nmutStringsFrom1To20 = %d\nnumbers = %d", [strings retainCount], [mutStrings retainCount], [empty retainCount], [stringsFrom1To20 retainCount], [shallowCopyForStrings retainCount], [oneLevelDeepCopyForStringsArr retainCount], [trueDeepCopyForStrings retainCount], [mutStringsFrom1To20 retainCount], [numbers retainCount]);
     
     //Releasing the objects
     [shallowCopyForStrings release];
